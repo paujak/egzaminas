0158794b764b31a3efdf1ad271cfcda39da8219c
Paulius <paujak@gmail.com>
2021-06-22 10:43:58 +0300

    Entities added

---
+++ sandelys-back/src/main/java/it/akademija/paujak/sandelys/entity/Inventorius.java
@@ -0,0 +1,133 @@
+package it.akademija.paujak.sandelys.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "inventorius")
+public class Inventorius implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotBlank(message = "Inventoriaus pavadinimas būtinas")
+	private String pavadinimas;
+	@NotBlank(message = "Būtina nurodyti inventoriaus svorį")
+	private double svoris;
+	@NotBlank(message = "Būtina nurodyti inventoriaus sektorių (1-40)")
+	@Min(value = 1, message= "Negali būti mažesnis už 1")
+	@Max(value = 40, message = "Negali būti didesnis už 40")
+	private int sektorius;
+	private LocalDate talpinimoData;
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "klientas_id")
+	private Klientas klientas;
+	
+	public Klientas getKlientas() {
+		return klientas;
+	}
+	public void setKlientas(Klientas klientas) {
+		this.klientas = klientas;
+	}
+	public Inventorius() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPavadinimas() {
+		return pavadinimas;
+	}
+	public void setPavadinimas(String pavadinimas) {
+		this.pavadinimas = pavadinimas;
+	}
+	public double getSvoris() {
+		return svoris;
+	}
+	public void setSvoris(double svoris) {
+		this.svoris = svoris;
+	}
+	public int getSektorius() {
+		return sektorius;
+	}
+	public void setSektorius(int sektorius) {
+		this.sektorius = sektorius;
+	}
+	public LocalDate getTalpinimoData() {
+		return talpinimoData;
+	}
+	public void setTalpinimoData(LocalDate talpinimoData) {
+		this.talpinimoData = talpinimoData;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((klientas == null) ? 0 : klientas.hashCode());
+		result = prime * result + ((pavadinimas == null) ? 0 : pavadinimas.hashCode());
+		result = prime * result + sektorius;
+		long temp;
+		temp = Double.doubleToLongBits(svoris);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((talpinimoData == null) ? 0 : talpinimoData.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Inventorius other = (Inventorius) obj;
+		if (klientas == null) {
+			if (other.klientas != null) {
+				return false;
+			}
+		} else if (!klientas.equals(other.klientas)) {
+			return false;
+		}
+		if (pavadinimas == null) {
+			if (other.pavadinimas != null) {
+				return false;
+			}
+		} else if (!pavadinimas.equals(other.pavadinimas)) {
+			return false;
+		}
+		if (sektorius != other.sektorius) {
+			return false;
+		}
+		if (Double.doubleToLongBits(svoris) != Double.doubleToLongBits(other.svoris)) {
+			return false;
+		}
+		if (talpinimoData == null) {
+			if (other.talpinimoData != null) {
+				return false;
+			}
+		} else if (!talpinimoData.equals(other.talpinimoData)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+}
--- sandelys-back/src/main/java/it/akademija/paujak/sandelys/entity/Klientas.java
+++ sandelys-back/src/main/java/it/akademija/paujak/sandelys/entity/Klientas.java
@@ -1,25 +1,131 @@
 package it.akademija.paujak.sandelys.entity;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
 
 import it.akademija.paujak.sandelys.enums.KlientoTipas;
 
 @Entity
-@Table(name = "klientai")
-public class Klientas {
+@Table(name = "klientai",
+uniqueConstraints = { @UniqueConstraint(columnNames = { "vardas", "pavarde", "gimimoData" }) })
+public class Klientas implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@NotBlank
 	private String vardas;
+	@NotBlank
 	private String pavarde;
+	@NotBlank
 	private LocalDate gimimoData;
+	@NotBlank
 	private String telNumeris;
+	@NotBlank
 	private KlientoTipas tipas;
+	@OneToMany(mappedBy = "klientas", cascade = CascadeType.ALL)
+	private List<Inventorius> inventoriausSarasas;
+	
+	public Klientas() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getVardas() {
+		return vardas;
+	}
+	public void setVardas(String vardas) {
+		this.vardas = vardas;
+	}
+	public String getPavarde() {
+		return pavarde;
+	}
+	public void setPavarde(String pavarde) {
+		this.pavarde = pavarde;
+	}
+	public LocalDate getGimimoData() {
+		return gimimoData;
+	}
+	public void setGimimoData(LocalDate gimimoData) {
+		this.gimimoData = gimimoData;
+	}
+	public String getTelNumeris() {
+		return telNumeris;
+	}
+	public void setTelNumeris(String telNumeris) {
+		this.telNumeris = telNumeris;
+	}
+	public KlientoTipas getTipas() {
+		return tipas;
+	}
+	public void setTipas(KlientoTipas tipas) {
+		this.tipas = tipas;
+	}
+	public List<Inventorius> getInventoriausSarasas() {
+		return inventoriausSarasas;
+	}
+	public void setInventoriausSarasas(List<Inventorius> inventoriausSarasas) {
+		this.inventoriausSarasas = inventoriausSarasas;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((gimimoData == null) ? 0 : gimimoData.hashCode());
+		result = prime * result + ((pavarde == null) ? 0 : pavarde.hashCode());
+		result = prime * result + ((vardas == null) ? 0 : vardas.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Klientas other = (Klientas) obj;
+		if (gimimoData == null) {
+			if (other.gimimoData != null) {
+				return false;
+			}
+		} else if (!gimimoData.equals(other.gimimoData)) {
+			return false;
+		}
+		if (pavarde == null) {
+			if (other.pavarde != null) {
+				return false;
+			}
+		} else if (!pavarde.equals(other.pavarde)) {
+			return false;
+		}
+		if (vardas == null) {
+			if (other.vardas != null) {
+				return false;
+			}
+		} else if (!vardas.equals(other.vardas)) {
+			return false;
+		}
+		return true;
+	}
 
+	
+	
 }
